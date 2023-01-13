96dad7bd3762f22b3120181e6edc4b70df7d2600
ayush-devshark <67559031+ayush-devshark@users.noreply.github.com>
2023-01-13 11:18:02 +0530

    add to notes

--- 2/App.js
+++ 2/App.js
@@ -1,8 +1,8 @@
-import React from  "react";  
+import React from 'react';
 import ReacDOM from 'react-dom/client';
 
-
-const heading = React.createElement('h1',{id:'heading'}, 'Hello World');
+const heading = React.createElement('h1', { id: 'heading' }, 'Hello World');
+const heading2 = React.createElement('h3', {}, 'Hello World');
+const container = React.createElement('div', {}, heading);
 const root = ReacDOM.createRoot(document.getElementById('root'));
-root.render(heading);
-
+root.render(container);
